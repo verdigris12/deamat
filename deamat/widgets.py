39eeb081a520035b91e8ac06281bbef8cc121e64
verdigris <verdigris1903@proton.me>
2024-08-19 21:55:54 +0300

    feat: pickle figure and open in new pyplot process on button click

--- deamat/widgets.py
+++ deamat/widgets.py
@@ -2,7 +2,15 @@ import matplotlib
 import imgui
 import imgui_datascience as imgui_ds
 from imgui_bundle import portable_file_dialogs as pfd
+import pickle
+import multiprocessing
+import matplotlib.pyplot as plt
 
+
+def open_figure_in_pyplot(pickled_figure):
+    fig = pickle.loads(pickled_figure)
+    plt.figure(fig.number)
+    plt.show()
 
 def im_plot_figure(state, figname, width=None, height=None, autosize=False):
     fig_obj = state.figures[figname]
@@ -20,7 +28,9 @@ def im_plot_figure(state, figname, width=None, height=None, autosize=False):
     if imgui.button('Plot ' + title):
         state.invalidate_figure(figname)
     if imgui.button('Open in pyplot'):
-        pass
+        pickled_figure = pickle.dumps(figure)
+        p = multiprocessing.Process(target=open_figure_in_pyplot, args=(pickled_figure,))
+        p.start()
     imgui.same_line()
     if imgui.button('Save figure'):
         fpath = pfd.save_file(title + '.png', state.figure_path).result()
