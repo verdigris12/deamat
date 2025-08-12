daf5b17a8aa26f3db176b00608ec1d8be541dde9
verdigris <verdigris1903@proton.me>
2025-08-12 14:31:42 +0000

    Updated flake for using with UV

--- flake.nix
+++ flake.nix
@@ -1,67 +1,60 @@
 {
-  description = "Deamat";
+  description = "Deamat: imgui+matplotlib boilerplate";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/e2605d0744c2417b09f8bf850dfca42fcf537d34";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    title = "Deamat";
-  in
-  {
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        devshellName = "PYTHON";
 
-    devShells.${system}.default = pkgs.mkShell {
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-        zlib
-        zstd
-        stdenv.cc.cc
-        curl
-        openssl
-        attr
-        libssh
-        bzip2
-        libxml2
-        acl
-        libsodium
-        util-linux
-        xz
-        # For IMGUI
-        xorg.libXext
-        xorg.libX11
-        libGL
-        libGLU
-        glib
-      ]);
+        pkgs = import nixpkgs { inherit system; };
 
-      buildInputs = with pkgs; [
-        zlib
-        zstd
-        stdenv.cc.cc
-        curl
-        openssl
-        attr
-        libssh
-        bzip2
-        libxml2
-        acl
-        libsodium
-        util-linux
-        xz
-        systemd
-        (pkgs.python311.withPackages (ps: with ps; [
-          pysocks
-          pip
-          pyopengl
-          matplotlib
-         ]))
-      ];
+        pythonNative = pkgs.python311.withPackages (ps: [
+          ps.numpy
+          ps.lxml
+          ps.pysocks
+          ps.ipython
+          ps.setuptools
+          ps.wheel
+        ]);
 
-      shellHook = ''
-        export DEVSHELL="${title}"
-        '';
-    };
-  };
+        nativeTools = with pkgs; [ gcc cmake ninja pkg-config ];
+
+        # runtime libraries (zlib + OpenSSL + GCC’s libstdc++ + others)
+        # used for building backends
+        nativeLibs = with pkgs; [
+          zlib
+          openssl
+          (pkgs.lib.getLib pkgs.stdenv.cc.cc)
+          # for numpy
+          blas lapack gfortran
+          # For imgui
+          libGL xorg.libX11 xorg.libXi xorg.libXrandr mesa xorg.libXext libGLU
+        ];
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ pythonNative pkgs.uv ] ++ nativeTools ++ nativeLibs;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeLibs;
+
+          shellHook = ''
+          export DEVSHELL=${devshellName}
+          export UV_NO_BINARY=1
+          export UV_NO_BUILD_ISOLATION=1
+
+          if [ ! -d .venv ]; then
+            "${pythonNative}/bin/python" -m venv .venv
+          fi
+          source .venv/bin/activate
+          export UV_PYTHON="$VIRTUAL_ENV/bin/python"
+
+          echo "($(python -V)) venv active → use: uv pip install <pkg>"
+          .venv/bin/pip install --upgrade pip setuptools wheel >/dev/null
+          '';
+        };
+      });
 }
